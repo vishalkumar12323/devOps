8ef82e7da60a966ed47569cf057dda52d13c2682
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-07-26 22:36:31 +0530

    test with scp command

--- commands.sh
+++ commands.sh
@@ -4,4 +4,8 @@
 # Upload a file (Local -> Remote)
 # scp /paht/to/local/file.txt username@remote_ip:/path/to/remote/directory
 
-scp -i "SSH-LOOKUP" "C:/Users/me/Download/flower.jpg" ubuntu@110.0.1.230:/home/me
+scp -i "SSH-LOOKUP" "C:/Users/me/Download/flower.jpg" ubuntu@13.233.79.108:/home/me
+
+
+# Download a file (Remote -> Local)
+scp ubuntu@13.233.79.108:/home/me/Downloads/access_key.pem ~/Downlaods
