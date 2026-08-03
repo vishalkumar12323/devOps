60f4a84a3d030b47608046a80e3b3ba838e8f43c
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-08-03 23:11:48 +0530

    added apt command uses!

---
+++ linux/apt.sh
@@ -0,0 +1,14 @@
+# The APT(Advance Package Tool) Command -> A command line utility used to install, update, remove and manage software packages on Ubuntu/Debian, and other debian based Linux-distributions.
+
+# Common Apt commands
+sudo apt update # Refreshes the list of available software and versions.
+
+sudo apt upgrade # Install the newest versions of all current apps.
+
+sudo apt install <package> # Download and install specific package.
+
+sudo apt remove <package> #  Unistalls a specific program but keep config. files, use "purge" wipe config. files
+
+sudo apt autoremove # Delete residual software libaries no longer needed.
+
+apt search <keyword> # Looks through the store database for an app matching the term
