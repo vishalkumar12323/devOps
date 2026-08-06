c52df0becadea3f9cae91f36faabeb41cc41ec0e
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-08-06 15:36:02 +0530

    added find command uses!

--- linux/find.sh
+++ linux/find.sh
@@ -29,3 +29,18 @@ find [starting_point] [options] [expression]
     # Find sybolic links
     find . -type l
 # **/
+
+
+# Find the all logs files in /var/log dir and make them all empty
+find /var/log -type f -name "*.log" -exec truncate -s 0 {} +
+
+# find -> utility command
+# /var/log -> searching directory
+# -type f -> search object type file
+# -name "*.log" -> all file that ext. ended with .log
+# -exec -> The pipe command redirect find command output to truncate command
+# truncate -> utility command
+# -s -> option refering to file size
+# 0 -> size 0 (zero)
+# {} -> find command result replaces the breakates
+# + -> end sign
