383f2fc95088a75fca4a0c75c2ed9d1bcbfaeb4e
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-08-07 22:35:24 +0530

    adding systemctl command!

---
+++ linux/systemctl.sh
@@ -0,0 +1,11 @@
+
+sudo apt install apache2
+
+systemctl status apache2
+
+sudo systemctl enable apache2
+sudo systemctl disable apache2
+
+
+sudo systemctl start apache2
+sudo systemctl stop apache2
