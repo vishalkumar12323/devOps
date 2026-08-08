152017c439824f8d9352f18cabda5a64cfaab1b7
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-08-08 17:31:13 +0530

    added curd operation commands

--- linux/user-management.sh
+++ linux/user-management.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# ==================================================
 # Checking existing users
 cat /etc/passwd # Provide list of all users currently exist in the system
 cat /etc/passwd | wc -l # Give Users count
 
 
 ls -l /home # Bonus but not recommended
+# ==================================================
 
 
 
+
+# ==================================================
 # Create New User
 sudo useradd guest
 
@@ -16,13 +20,28 @@ cat /etc/passwd
 # usrname  usrId        path      shell
 # guest:x:1001:1002::/home/guest:/bin/sh
 
-
 # Delete User
 sudo userdel guest
+# ==================================================
 
 
+
+
+
+# ==================================================
 # Create user with home directory
 sudo useradd -m smith # create user smith and add to the /home dir.
 
 # Remove user and also remove from the /home dir.
 sudo userdel -r smith
+# ===================================================
+
+
+
+# ==================================================
+# Adding or updating user password
+sudo passwd guest
+
+
+# Create system user
+sudo useradd -r sysuser
