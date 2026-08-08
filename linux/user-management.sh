0b4d304c3c0eddf90cfb26e56e4bcdf6e3e4a7f7
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-08-08 16:49:47 +0530

    added curd operation commands

--- linux/user-management.sh
+++ linux/user-management.sh
@@ -10,4 +10,19 @@ ls -l /home # Bonus but not recommended
 
 
 # Create New User
-sudo useradd <username>
+sudo useradd guest
+
+cat /etc/passwd
+# usrname  usrId        path      shell
+# guest:x:1001:1002::/home/guest:/bin/sh
+
+
+# Delete User
+sudo userdel guest
+
+
+# Create user with home directory
+sudo useradd -m smith # create user smith and add to the /home dir.
+
+# Remove user and also remove from the /home dir.
+sudo userdel -r smith
