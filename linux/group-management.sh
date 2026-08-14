fc0c6eb2b2a692fc350d89dd6c381ee34a063fdc
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-08-14 17:16:56 +0530

    Added CURD commands!

--- linux/group-management.sh
+++ linux/group-management.sh
@@ -8,3 +8,9 @@ cat /etc/group # shows all groups and their members
 
 sudo groupadd <groupname> # creates a new group
 sudo groupdel <groupname> # deletes a group
+
+
+sudo usermod -aG <groupname> <username> # adds a user to a group
+sudo gpasswd -a <username> <groupname> # adds a user to a group
+sudo gpasswd -d <username> <groupname> # removes a user from a group
+
