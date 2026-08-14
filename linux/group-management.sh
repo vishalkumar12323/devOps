dceb692e829307adbfc74ef1de9f411ca9954560
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-08-14 16:44:01 +0530

    Added CURD commands!

--- linux/group-management.sh
+++ linux/group-management.sh
@@ -4,3 +4,7 @@ groups # shows all available groups on the system
 groups <username> # shows all groups for a specific user
 
 cat /etc/group # shows all groups and their members
+
+
+sudo groupadd <groupname> # creates a new group
+sudo groupdel <groupname> # deletes a group
